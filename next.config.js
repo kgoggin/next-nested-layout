1f733b675fa5b49f3c39c4a414854379ab4b6e87
Kyle Goggin <kyle@kylegoggin.com>
2019-08-07 09:51:38 -0400

    added exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const withMDX = require("@zeit/next-mdx")({
 });
 const withCSS = require("@zeit/next-css");
 
-module.exports = withCSS(withMDX());
+module.exports = withCSS(
+  withMDX({
+    exportPathMap: () => ({
+      "/": { page: "/" },
+      "/docs/page-1": { page: "/docs/[id]", query: { id: "page-1" } },
+      "/docs/page-2": { page: "/docs/[id]", query: { id: "page-2" } },
+      "/examples/page-1": { page: "/examples/[id]", query: { id: "page-1" } },
+      "/examples/page-2": { page: "/examples/[id]", query: { id: "page-2" } }
+    })
+  })
+);
